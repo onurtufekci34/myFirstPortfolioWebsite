413e213c4c81ec2efcb3af2113a7e6db397a9cb2
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-10 18:45:36 +0100

    update

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let date = new Date;
 let year = date.getFullYear();
 console.log(year);
 document.getElementById("year").innerHTML = year;
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var img = document.getElementById("myPhoto");
+    img.src = img.getAttribute("data-src");
+  });
+
